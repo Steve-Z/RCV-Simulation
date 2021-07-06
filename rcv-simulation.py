faf71fd8aa0dd4017056088ee7fe1164e826cedb
Steven Zekowski <steve.z@tutanota.com>
2021-07-06 16:22:47 -0500

    Show ballots that did not pick either finalist

--- rcv-simulation.py
+++ rcv-simulation.py
@@ -1,8 +1,10 @@
 from rcv import RCV
+from itertools import filterfalse
 
 r = RCV()
 
-ballots = r.ballots(fieldsize=10, turnout=10000)
+ballots = r.ballots(fieldsize=7, turnout=1001)
+originals = ballots[:]
 
 round1 = r.count(ballots)
 
@@ -13,4 +15,19 @@ for k, v in round1[0].items():
 
 print("Total:", sum(round1[0].values()))
 
-r.runoff(first=round1[1], last=round1[2], d=round1[0], rnd=2, ballots=ballots)
+d = r.runoff(first=round1[1], last=round1[2], d=round1[0], rnd=2, ballots=ballots)
+print(d.keys())
+print(len(originals))
+
+
+def uncounted(alist):
+    tests = []
+    for k in d.keys():
+        test = k in alist
+        tests.append(test)
+    return any(tests)
+
+
+wasted = list(filterfalse(uncounted, originals))
+print(wasted)
+print(len(wasted))
