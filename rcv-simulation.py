f094ee85631e10fd4bb57eda0058f6b5b6d74277
Steven Zekowski <steve.z@tutanota.com>
2021-07-06 17:35:12 -0500

    Minor edits

--- rcv-simulation.py
+++ rcv-simulation.py
@@ -3,7 +3,7 @@ from itertools import filterfalse
 
 r = RCV()
 
-ballots = r.ballots()
+ballots = r.ballots(turnout=10000)
 originals = ballots[:]
 
 round1 = r.count(ballots)
@@ -17,8 +17,8 @@ print("Total:", sum(round1[0].values()))
 
 d = r.runoff(
     first=round1[1], last=round1[2], d=round1[0], rnd=2, ballots=ballots)
-print(*list(d.keys()), "were the finalists.")
-print(len(originals), "original ballots cast.")
+print(', '.join(list(d.keys())), "were the finalists.")
+print(len(originals), "first-round were ballots cast.")
 
 
 def uncounted(alist):
